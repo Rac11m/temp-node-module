4640bc0e914429352fdf1d423515b9b3ffcb724c
IamUnkn0wn <racimsaal@gmail.com>
2022-09-06 22:40:44 +0100

    first commit

---
+++ altFlavour.js
@@ -0,0 +1,6 @@
+module.exports.items = ["gitem1", "item2"];
+const person = {
+  name: "john",
+};
+
+module.exports.singlePerson = person;
---
+++ names.js
@@ -0,0 +1,8 @@
+// local
+const secret = 11;
+// share
+
+const john = "john";
+const peter = "peter";
+
+module.exports = { john, peter };
